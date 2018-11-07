df29ee60e2a9ecdac269601f88602e664f65c4f3
woobin <woobin0413@gmail.com>
2018-11-07 00:40:18 -0600

    merge sort
    Inversions count

---
+++ src/CountInversions.java
@@ -0,0 +1,26 @@
+
+public class CountInversions {
+
+	static int arr[] = new int[] { 1, 20, 6, 4, 5 }; 
+	
+	static int getInvCount(int n) {
+		int count = 0;
+		for (int i = 0; i < n-1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if(arr[i]>arr[j]) {
+					count++;
+				}
+			}
+		}
+	return count;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Number of inversions are "
+                  + getInvCount(arr.length)); 
+	}
+
+}
