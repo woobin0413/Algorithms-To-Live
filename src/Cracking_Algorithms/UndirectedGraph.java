7e6c46902424b77788dbb35b700a9c70b3cae8c3
woobin0413 <woobin0413@gmail.com>
2018-11-09 20:45:12 -0600

    undirected and weighted graph

--- src/Cracking_Algorithms/UndirectedGraph.java
+++ src/Cracking_Algorithms/UndirectedGraph.java
@@ -1,4 +1,11 @@
 package Cracking_Algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 //https://blog.naver.com/metalingus58/221375775247
 //https://algorithms.tutorialhorizon.com/weighted-graph-implementation-java/
 
@@ -11,11 +18,42 @@ package Cracking_Algorithms;
  */
 public class UndirectedGraph {
 
-
-	public static void main(String[] args) {
-		
+	static int v, e;
+	static ArrayList<Integer>[] con, conv;
 	
-
+	public static void main(String[] args) throws Exception {
+		
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	//특정 문자로 구분되어있는 문자열을 아주 쉽게 처리할 수 있다
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	//v = node, e = edge
+	v = Integer.parseInt(st.nextToken());
+	e = Integer.parseInt(st.nextToken());
+	
+	con = new ArrayList[v+1];
+	conv = new ArrayList[v+1];
+	
+	//Node 만큼 array 생성
+	for (int i = 1; i<=v; i++) {
+		con[i] = new ArrayList<>();
+		conv[i] = new ArrayList<>();
+	}
+	//Edge 만들때
+	//a->b, b->a
+	for (int i = 1; i <= e; i++) {
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st1.nextToken());
+		int b = Integer.parseInt(st1.nextToken());
+		int c = Integer.parseInt(st1.nextToken());
+		//conv 는 가중치 관리용 arraylist 이며
+		//출발점, 도착점 가중치 모두 관리된다.
+		con[a].add(b);
+		conv[a].add(c);
+		
+		con[b].add(a);
+		conv[b].add(c);
+	}
+	
 	}
 }
 
