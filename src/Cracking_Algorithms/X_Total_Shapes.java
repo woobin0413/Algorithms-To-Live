8ab963ea8baf1dd2f5102dedc2c0c996652f12cc
woobin <woobin0413@gmail.com>
2018-11-11 19:20:44 -0600

    fixed

--- src/Cracking_Algorithms/X_Total_Shapes.java
+++ src/Cracking_Algorithms/X_Total_Shapes.java
@@ -11,17 +11,21 @@ public class X_Total_Shapes
  {
      
 	  public static void main (String[] args) throws IOException {
+		  //bufferedReader using as scanner
 		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		  System.out.print("Type the number of arrays : ");
 		  int arrayNum = Integer.parseInt(br.readLine());
-		  System.out.println(arrayNum);
+		  
 		  for (int i = 0; i < arrayNum; i++) {
+			System.out.print("/n"+"vertical matrix : ");
 			int vert = Integer.parseInt(br.readLine());
+			System.out.print("/n"+"horizontal matrix : ");
 			int horizon = Integer.parseInt(br.readLine());
 			
 			char[][] arr = new char [vert][horizon];
 			
 			for (int j = 0; j < vert; j++) {
+				System.out.print("/n"+"X shapes : ");
 				char [] eachArr = br.readLine().toCharArray();
 				for (int k = 0; k < horizon; k++) {
 					arr[j][k] = eachArr[k];
