43ff529edf4f5f492db2a43f3a3d4ef706d4d642
woobin <woobin0413@gmail.com>
2018-11-19 00:45:52 -0600

    naive solution
    time complexitiy = O(n^2)

--- src/Cracking_Algorithms/SubArrays_with_zero.java
+++ src/Cracking_Algorithms/SubArrays_with_zero.java
@@ -9,17 +9,27 @@ import java.io.*;
 //Input { 4,2,-3,-1,0,4};
 //Ouput Subarray with 0 sum exists
 //ex: {-3,-1,0,4}, {0}
-
+//APPROACH 1: Naive Solution
 public class SubArrays_with_zero {
 
 	public static void calculation(int[] arr) {
 		
+		int term = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for (int j = i; j < arr.length; j++) {
+				sum += arr[j];
+				if(sum == 0){
+					System.out.println(i + ", " + j);
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		Map m1 = new HashMap(); 
 	
-		int[] arr = { 4,2,-3,-1,0,4};
+		int[] arr = { 4,2,-3,-1,0,4,5,3,-8};
 		
 		calculation(arr);
 
