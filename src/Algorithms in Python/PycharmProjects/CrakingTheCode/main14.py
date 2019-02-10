e6a658e273933c07b2e31c0fa3053b29ea870d9d
woobin <woobin0413@gmail.com>
2019-02-10 03:48:10 -0600

    mains

--- src/Algorithms in Python/PycharmProjects/CrakingTheCode/main14.py
+++ src/Algorithms in Python/PycharmProjects/CrakingTheCode/main14.py
@@ -1,2 +1,27 @@
 # -*- coding: utf-8 -*-
-# https://practice.geeksforgeeks.org/problems/size-of-binary-tree/1/?ref=self
+# https://practice.geeksforgeeks.org/problems/inorder-traversal/1/?ref=self
+
+# Non-Recursive answer
+def inorderTraversal(root):
+    temp_node = root
+    stack = []
+
+    while stack or temp_node:
+        if (temp_node):
+            stack.append(temp_node)
+            temp_node = temp_node.left
+        else:
+            temp_node = stack[len(stack) - 1]
+            stack.pop()
+
+            print (temp_node.data, end = " ")
+
+            temp_node = temp_node.right
+
+# Recrusive answer
+
+def inorderTraversal(root):
+    if(root):
+        inorderTraversal(root.left)
+        print(root.data, end = " ")
+        inorderTraversal(root.right)
---
+++ src/Algorithms in Python/PycharmProjects/CrakingTheCode/main15.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# https://practice.geeksforgeeks.org/problems/preorder-traversal/1/?ref=self
