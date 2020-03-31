4799b59846c51392ceb8cad76688ff7acc04df0e
Woobin Chung <wchung@boostb2b.com>
2020-03-30 23:00:11 -0400

    1365
    How Many Numbers Are Smaller Than the Current Number

---
+++ src/Let's go Amazon/1365.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Let_s_go_Amazon
+{
+    //https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    class _1365
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] list = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] != nums[j] && nums[i] > nums[j])
+                    {
+                        count++;
+                    }
+
+                }
+
+                list[i] = count;
+
+            }
+            return list;
+        }
+    }
+}
---
+++ src/Let's go Amazon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Let_s_go_Amazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
