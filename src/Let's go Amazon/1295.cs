e5b585e90ffaca06bb7ee3d3376ffb99306e8f3a
Woobin Chung <wchung@boostb2b.com>
2020-04-14 20:07:08 -0400

    Create 1295.cs

---
+++ src/Let's go Amazon/1295.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Let_s_go_Amazon
+{
+    class _1295
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int c = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((nums[i] > 9 && nums[i] < 100) || (nums[i] > 999 && nums[i] < 10000))
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+
+
+    }
+}
