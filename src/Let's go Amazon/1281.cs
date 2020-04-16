fc69cf72db9514884f3284247942b51eb4ca43c5
Woobin Chung <wchung@boostb2b.com>
2020-04-15 23:45:38 -0400

    Create 1281.cs

---
+++ src/Let's go Amazon/1281.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Let_s_go_Amazon
+{
+    class _1281
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            int sum = 0;
+            int product = 0;
+            for (int i = 0; i < n.ToString().Length; i++)
+            {
+                char num = n.ToString()[i];
+                sum += Int32.Parse(num.ToString());
+                product *= Int32.Parse(num.ToString());
+                
+                
+            }
+            return product - sum;
+        }
+
+    }
+}
